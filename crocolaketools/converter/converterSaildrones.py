0d2f941d70311bc0e7958a6020a607b6b2a01296
david11133 <davidnady4yad@gmail.com>
2025-06-30 20:12:31 +0300

    remove unimportant sensors

--- crocolaketools/converter/converterSaildrones.py
+++ crocolaketools/converter/converterSaildrones.py
@@ -123,14 +123,11 @@ class ConverterSaildrones(Converter):
             "TEMP_DEPTH_HALFMETER_MEAN": 0.5, 
             "O2_CONC_MEAN":              0.6,
             "O2_RBR_CONC_MEAN":          0.53,
-            "O2_CONC_RBR_MEAN":          0.53,
             "O2_CONC_SBE37_MEAN":        1.7, 
             "O2_CONC_UNCOR_MEAN":        0.6,
             "SAL_MEAN":                  0.6,
-            "SAL_RBR_MEAN":              0.53,
             "SAL_SBE37_MEAN":            1.7, 
             "CHLOR_MEAN":                0.25,
-            "CHLOR_RBR_MEAN":            0.53,
             "CHLOR_WETLABS_MEAN":        1.9, 
             "CDOM_MEAN":                 1.9, 
             "BKSCT_RED_MEAN":            1.9, 
@@ -207,7 +204,7 @@ class ConverterSaildrones(Converter):
         df["PRES"] = df["PRES"].astype("float32[pyarrow]")
 
         # Merge temperature readings from multiple sensors into a unified 'TEMP' column.
-        temp_sources = ["TEMP_SBE37_MEAN", "TEMP_DEPTH_HALFMETER_MEAN", "TEMP_CTD_MEAN", "TEMP_CTD_RBR_MEAN"]
+        temp_sources = ["TEMP_SBE37_MEAN", "TEMP_DEPTH_HALFMETER_MEAN", "TEMP_CTD_MEAN"]
         existing_temp_sources = [col for col in temp_sources if col in df.columns]
         df["TEMP"] = df[existing_temp_sources].bfill(axis=1).iloc[:, 0]
 
