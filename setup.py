6310b38e3a9cc6a5644c5f576d0bf976f2e51437
Enrico <enrico.milanese@whoi.edu>
2026-09-11 08:58:26 -0400

    bump to v1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def parse_requirements(filename):
 
 setup(
     name='crocolaketools',
-    version='1.0.1',
+    version='1.1.0',
     description='Package to generate and load CrocoLake',
     license="GNU GPLv3",
     long_description=long_description,
